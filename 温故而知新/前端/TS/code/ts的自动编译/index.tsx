a7a38e100b1c69d1dd6494a97e5ff21662bfe052
WangNing-Zhou <1879154660@qq.com>
2023-03-28 14:27:34 +0800

    前端 状态机

---
+++ 温故而知新/前端/TS/code/ts的自动编译/index.tsx
@@ -0,0 +1,17 @@
+(()=>{
+    function sayHi(str){
+        return '你好'+str
+    }
+    sayHi('周周')
+})()
+
+//类型主接: 是一种轻量级的为函数或者变量添加的约束
+
+function showMsg(str: string){
+
+}
+
+let msg = [10,20.30]
+
+// console.log(showMsg(msg))
+
