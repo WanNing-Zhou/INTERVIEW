91c50dda3f3c7490f1bc209f8a927056b7c09181
WangNing-Zhou <1879154660@qq.com>
2023-06-15 14:02:06 +0800

    腾讯云面试面经

---
+++ 前端面试mark/src/Vue代码复习/my-vue-app/src/components/ChildrenComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="child">
+    <h1>这是孩字组件</h1>
+  </div>
+</template>
+
+<style scoped lang="less">
+  .child{
+    width: 300px;
+    height: 300px;
+    background: #535bf2;
+  }
+</style>
---
+++ 前端面试mark/src/Vue代码复习/my-vue-app/src/components/FatherComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import ChildrenComponent from "./ChildrenComponent.vue";
+import {onMounted} from "vue";
+
+</script>
+
+<template>
+  <div class="father">
+    <h1>这是父组件</h1>
+    <children-component></children-component>
+  </div>
+
+</template>
+
+<style scoped lang="less">
+.father{
+  width: 500px;
+  height: 500px;
+  background: red;
+
+}
+</style>
