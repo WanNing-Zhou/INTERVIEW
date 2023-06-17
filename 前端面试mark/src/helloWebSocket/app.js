2513e03c1e84cddddce6701184f7995fb02d5626
WangNing-Zhou <1879154660@qq.com>
2023-06-17 21:03:11 +0800

    面试总结

---
+++ 前端面试mark/src/helloWebSocket/app.js
@@ -0,0 +1,25 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', (socket) => {
+    // 处理连接请求
+    console.log('Client connected');
+
+    // 处理消息
+    socket.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+
+        // 广播消息给所有客户端
+        server.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    // 处理断开连接
+    socket.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
